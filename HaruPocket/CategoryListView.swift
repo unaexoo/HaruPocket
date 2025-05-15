545558e74b33e8325ca32e8e18983c9634a0dda9
jihyeonjjang <jihyeon9975@gmail.com>
2025-05-15 20:21:43 +0900

    Feat: List를 LazyVGrid로 변경

--- HaruPocket/CategoryListView.swift
+++ HaruPocket/CategoryListView.swift
@@ -16,7 +16,7 @@ import SwiftData
 struct CategoryListView: View {
     @Environment(\.modelContext) private var context
     @AppStorage("username") private var username: String = "default_user"
-    
+
     @EnvironmentObject var spendingViewModel: SpendingViewModel
 
     @Environment(\.dismiss) var dismiss
@@ -24,80 +24,98 @@ struct CategoryListView: View {
     // 수정사항
     @Binding var category: Category?
 
+    private let columns = [
+        GridItem(.adaptive(minimum: 200, maximum: .infinity), spacing: nil, alignment: nil),
+    ]
+
     var body: some View {
-        List {
-            // 필터링 조건:
-            // - category가 nil이 아닌 경우:
-            //     해당 사용자의 항목 중에서 category 이름이 현재 선택된 category와 일치하는 항목만 필터링
-            // - category가 nil인 경우:
-            //     사용자 ID만 확인하여 전체 소비 항목을 필터링 없이 모두 보여줌 (전체 보기 용도)
-            let filtereditems = spendingViewModel.spending
-                .filter { category != nil ? ($0.userID == spendingViewModel.username) && ($0.category?.name == category?.name) : ($0.userID == spendingViewModel.username) }
-            // 최신 날짜가 먼저 오도록 정렬
-                .sorted { $0.date > $1.date }
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                // 필터링 조건:
+                // - category가 nil이 아닌 경우:
+                //     해당 사용자의 항목 중에서 category 이름이 현재 선택된 category와 일치하는 항목만 필터링
+                // - category가 nil인 경우:
+                //     사용자 ID만 확인하여 전체 소비 항목을 필터링 없이 모두 보여줌 (전체 보기 용도)
+                let filtereditems = spendingViewModel.spending
+                    .filter { category != nil ? ($0.userID == spendingViewModel.username) && ($0.category?.name == category?.name) : ($0.userID == spendingViewModel.username) }
+                // 최신 날짜가 먼저 오도록 정렬
+                    .sorted { $0.date > $1.date }
 
-            ForEach(filtereditems) { item in
-                if let index = spendingViewModel.spending.firstIndex(where: { $0.id == item.id }) {
-                    NavigationLink {
-                        DetailView(basics: $spendingViewModel.spending[index])
-                    } label: {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(item.title)
-                                // 날짜를 yyyy.MM.dd 형식으로 표시
-                                Text(formattedDate(item.date))
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                            }
-                            Spacer()
-                            Text("\(item.money)원")
-                                .foregroundColor(.gray)
-                        }
-                        .swipeActions(edge: .trailing) {
-                            Button(role: .destructive) {
-                                // SwiftData에서 해당 항목 삭제
-                                context.delete(item)
-                                try? context.save()
+                ForEach(filtereditems) { item in
+                    if let index = spendingViewModel.spending.firstIndex(where: { $0.id == item.id }) {
+                        NavigationLink {
+                            DetailView(basics: $spendingViewModel.spending[index])
+                        } label: {
+                            VStack(spacing: 0) {
+								Divider()
+                                    .padding(.horizontal, 20)
 
-                                // ViewModel에서도 제거
-                                spendingViewModel.spending.remove(at: index)
-                            } label: {
-                                Label("삭제", systemImage: "trash")
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(item.title)
+                                            .foregroundStyle(Color.primary)
+                                        
+                                        // 날짜를 yyyy.MM.dd 형식으로 표시
+                                        Text(formattedDate(item.date))
+                                            .font(.footnote)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text("\(item.money)원")
+                                    
+                                    Image(systemName: "chevron.forward")
+                                }
+                                .foregroundStyle(.gray)
+                                .padding(.top, 10)
+                                .padding(.horizontal, 20)
+                                .swipeActions(edge: .trailing) {
+                                    Button(role: .destructive) {
+                                        // SwiftData에서 해당 항목 삭제
+                                        context.delete(item)
+                                        try? context.save()
+                                        
+                                        // ViewModel에서도 제거
+                                        spendingViewModel.spending.remove(at: index)
+                                    } label: {
+                                        Label("삭제", systemImage: "trash")
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        .scrollIndicators(.hidden)
-        .listStyle(.plain)
-        .onAppear {
-            spendingViewModel.username = username
-            spendingViewModel.loadCategory(context: context)
-            spendingViewModel.loadEntry(context: context)
+            .scrollIndicators(.hidden)
+            .listStyle(.plain)
+            .onAppear {
+                spendingViewModel.username = username
+                spendingViewModel.loadCategory(context: context)
+                spendingViewModel.loadEntry(context: context)
 
-            Task {
-                await spendingViewModel.insertSampleData(context: context)
-            }
-        }
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.backward")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(Color.lightPointColor)
+                Task {
+                    await spendingViewModel.insertSampleData(context: context)
                 }
             }
-            ToolbarItem(placement: .principal) {
-                            Text(category?.name ?? "나의 모든 소비 기록")
-                                .font(.title2)
-                        }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(Color.lightPointColor)
+                    }
+                }
+                ToolbarItem(placement: .principal) {
+                    Text(category?.name ?? "나의 모든 소비 기록")
+                        .font(.title2)
+                }
+            }
         }
     }
 
@@ -129,10 +147,10 @@ struct CategoryListView: View {
 #Preview {
     NavigationStack {
         CategoryListView(category: .constant(nil))
-        .modelContainer(
-            for: [BasicEntry.self, Category.self],
-            inMemory: true
-        )
-        .environmentObject(SpendingViewModel())
+            .modelContainer(
+                for: [BasicEntry.self, Category.self],
+                inMemory: true
+            )
+            .environmentObject(SpendingViewModel())
     }
 }
